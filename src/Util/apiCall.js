ba0cb7ba55d4f8e1dfa8aa382ba80563789c44cd
Allison Wagner <allisonjw01@gmail.com>
2020-05-26 15:51:05 -0600

    Setting up api call and slicing out a limited amount of items

---
+++ src/Util/apiCall.js
@@ -0,0 +1,19 @@
+export const getProduct = async product => {
+    const url = `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const spliceData = data.splice(0, 30);
+  
+    const cleanedData = cleanProductData(spliceData);
+    if (!response.ok) {
+      throw new Error('Error fetching the makeup products');
+    }
+    return await Promise.all(cleanedData);
+}
+  
+const cleanProductData = (data) => {
+  return data.map(async result => {
+    const {id, brand, name, image_link, product_link, description, tag_list} = result;
+    return { id, brand, name, image_link, product_link, description, tag_list}
+  });
+};
