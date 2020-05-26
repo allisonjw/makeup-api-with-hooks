31de02fd2e83657d73f9b063c86eaa71833709a1
Allison Wagner <allisonjw01@gmail.com>
2020-05-26 15:58:11 -0600

    Setting up actions to be able to create state from api

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+export const addFoundations = foundation => ({
+    type: 'ADD_FOUNDATIONS',
+    foundation
+});
+
+export const addEyeShadows = eyeShadow => ({
+    type: 'ADD_EYESHADOW',
+    eyeShadow
+});
+
+export const addBlushes = blush => ({
+    type: 'ADD_BLUSH',
+    blush
+});
+
+export const addBronzers = bronzer => ({
+    type: 'ADD_BRONZERS',
+    bronzer
+});
+
+export const addLipsticks = lipstick => ({
+    type: 'ADD_LIPSTICKS',
+    lipstick
+});
+
+export const addMascaras = mascara => ({
+    type: 'ADD_MASCARA',
+    mascara
+});
