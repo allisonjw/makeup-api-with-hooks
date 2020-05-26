68fc32ddbd9debb19747c7975f81d7f67a1e6b64
Allison Wagner <allisonjw01@gmail.com>
2020-05-26 15:14:42 -0600

    Add node sass dependency and change css prefixes also adds skeleton to Nav

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import Nav from '../Nav/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export class App extends Component {
+
+
+
+  render() {
+    return (
+      <main className="App">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Makeup Api.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <Nav />
+     </main>
+    );
+  };
+};
 
 export default App;
---
+++ src/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Nav.scss';
+
+export const Nav = () => {
+    return (
+        <p>Nav</p>
+    )
+
+};
+
+export default Nav;
