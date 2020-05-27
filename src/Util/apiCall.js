093d47cfaf5c33c827b90643da29152960d3b059
Allison Wagner <allisonjw01@gmail.com>
2020-05-27 11:40:14 -0600

    Reduced amound of products being sliced out

--- src/Util/apiCall.js
+++ src/Util/apiCall.js
@@ -2,7 +2,7 @@ export const getProducts = async product => {
     const url = `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`;
     const response = await fetch(url);
     const data = await response.json();
-    const spliceData = data.splice(0, 30);
+    const spliceData = data.splice(0, 20);
   
     const cleanedData = cleanProductData(spliceData);
     if (!response.ok) {
