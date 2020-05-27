6f9b507d600359445570fbdae115969429c3f4fb
Allison Wagner <allisonjw01@gmail.com>
2020-05-27 11:40:46 -0600

    Refactored to be a function component with hooks

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,9 @@
-// import React, { Component } from 'react';
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch  } from 'react-redux';
 import './App.scss';
 import { getProducts } from '../Util/apiCall';
 import { addFoundations, addEyeShadows, addBlushes, addBronzers, addLipsticks, addMascaras, isLoading } from '../actions';
 import Nav from '../Nav/Nav';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -16,12 +13,12 @@ export const App = () => {
   const [bronzer, setAllBronzers] = useState([]);
   const [lipstick, setAllLipsticks] = useState([]);
   const [mascara, setAllMascaras] = useState([]);
-  useSelector(state => state.foundations)
-  useSelector(state => state.eyeShadows)
-  useSelector(state => state.blushes)
-  useSelector(state => state.bronzers)
-  useSelector(state => state.lipsticks)
-  useSelector(state => state.mascaras)
+  // const allFoundations = useSelector(state => state.foundations)
+  // useSelector(state => state.eyeShadows)
+  // useSelector(state => state.blushes)
+  // useSelector(state => state.bronzers)
+  // useSelector(state => state.lipsticks)
+  // useSelector(state => state.mascaras)
 
  const fetchProducts = async () => {
   try {
@@ -49,8 +46,8 @@ export const App = () => {
       const mascaras = await getProducts('mascara');
       setAllMascaras(mascaras);
       dispatch(addMascaras(mascaras));
-      isLoading(false)
-  } catch({ message }) {
+
+    } catch({ message }) {
       isLoading(false)
       console.log(message);
   }
@@ -59,34 +56,8 @@ export const App = () => {
     useEffect(() => {
       fetchProducts();
     }, []);
-// }
 
-// export class App extends Component {
-//   componentDidMount = async() => {
-//     const { addFoundations, addEyeShadows, addBlushes, addBronzers, addLipsticks, addMascaras } = this.props;
-//     try {
-//       isLoading(true);
-//       const foundations = await getProducts('foundation');
-//       addFoundations(foundations);
-//       const eyeShadows = await getProducts('eyeshadow');
-//       addEyeShadows(eyeShadows);
-//       const blushes = await getProducts('blush');
-//       addBlushes(blushes);
-//       const bronzers = await getProducts('bronzer');
-//       addBronzers(bronzers);
-//       const lipsticks = await getProducts('lipstick');
-//       addLipsticks(lipsticks);
-//       const mascaras = await getProducts('mascara');
-//       addMascaras(mascaras);
-//       isLoading(false)
-//     } catch({ message }) {
-//       isLoading(false)
-//       console.log(message);
-//     }
-//   }
-
-
-  // render() {
+    let allCategories = [...foundation, ...mascara, ...eyeShadow, ...blush, ...bronzer, ...lipstick];
     return (
       <main className="App">
         <p>
@@ -95,23 +66,6 @@ export const App = () => {
         <Nav />
      </main>
     );
-  // };
 };
 
 export default App;
-
-// export const mapStateToProps = state => ({
-//   blushes: state.blushes,
-//   bronzers: state.bronzers,
-//   eyeShadows: state.eyeShadows,
-//   foundations: state.foundations,
-//   lipsticks: state.lipsticks,
-//   mascaras: state.mascaras,
-//   isLoading: state.isLoading
-// });
-
-// export const mapDispatchToProps = dispatch => {
-//   return bindActionCreators({ addFoundations, addEyeShadows, addBlushes, addBronzers, addLipsticks, addMascaras, isLoading}, dispatch) 
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
