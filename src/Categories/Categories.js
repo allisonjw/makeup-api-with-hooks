d2dc2e1fec36295e26135d3defcb8360e7b5cc2f
Allison Wagner <allisonjw01@gmail.com>
2020-06-02 16:15:05 -0600

    Adds extra classnames and elements for styling

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import Product from '../Product/Product';
+import { Link } from 'react-router-dom';
 
 export const Categories = ({ productType, type, allCategoriesArray }) => {
     
    if (type === 'shopall') {
     const allCategoriesArrayList = allCategoriesArray.map(product => <Product key={product.id} {...product} />);
     return (
-      <section className='all_categories-section'>
-        {allCategoriesArrayList}
-      </section>
+      <>
+        <div className="back_btn-div">
+          <Link to='/'>
+            <button className='back_btn' >Back</button>
+          </Link>
+        </div>
+        <section className='all_categories-section'>
+          {allCategoriesArrayList}
+        </section>
+      </>
     )
    } else {
     const productList = productType.map(product => <Product key={product.id} {...product} />);
       return (
-        <section className='all_categories-section'>
-          {productList}
-        </section>
+        <>
+          <div className="back_btn-div">
+            <Link to='/'>
+              <button className='back_btn' >Back</button>
+            </Link>
+          </div>
+          <section className='all_categories-section'>
+            {productList}
+          </section>
+        </>
       )
     };
   };  
