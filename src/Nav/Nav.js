c4c6a44d8e41290f56191e058db9c837cc392dc1
Allison Wagner <allisonjw01@gmail.com>
2020-06-02 15:14:40 -0600

    Moves all links to products to be in Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Nav.scss';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Nav = () => {
     return (
@@ -11,17 +11,67 @@ export const Nav = () => {
                 <img className="counter_makeup-img" alt="makeup on counter" src={require('../Images/make-up-1209798_1280.png')}></img>
                 <img className="makeups-img" alt="various makeups" src={require('../Images/more_makeup.png')}></img>
                 <img className="lipstick-img" alt="lipstick" src={require('../Images/lipstick-791761_640.png')}></img>
-
             </div>
-            {/* <Link to='./products'>
-                        <button  
-                        className="house_card-btn" 
-                        type="submit">View Products
-                        </button>
-                    </Link> */}
-        </section>    
+        <section className="category_type-section">
+          <Link to={'products/foundation'}> 
+            <button 
+              className="foundation_btn"
+              type="submit"
+              name="foundation">
+              FOUNDATION
+            </button>
+          </Link>
+          <Link to="products/blush">
+            <button 
+              className="blush_btn"
+              type="submit"
+              name="blush">
+              BLUSH
+            </button>
+          </Link>
+          <Link to="/products/mascara">
+            <button 
+              className="mascara_btn"
+              type="submit"
+              name="mascara">
+              MASCARA
+            </button>
+          </Link>
+          <Link to="/products/eyeShadow">
+            <button 
+              className="eyeshadow_btn"
+              type="submit"
+              name="eyeshadow">
+              EYESHADOW
+            </button>
+          </Link>
+          <Link to="/products/lipstick">
+            <button 
+              className="lipstick_btn"
+              type="submit"
+              name="lipstick">
+              LIPSTICK
+            </button>
+          </Link>
+          <Link to="/products/bronzer">
+            <button 
+              className="bronzer_btn"
+              type="submit"
+              name="bronzer">
+              BRONZER
+            </button>
+          </Link>
+          <Link to="/shopall">
+            <button 
+              className="shopAll_btn"
+              type="submit"
+              name="shopAll">
+              SHOP ALL
+            </button>
+          </Link>
+       </section>
+       </section>    
     )
-
 };
 
 export default Nav;
