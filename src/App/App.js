9af591929354a8bc00606723e2d2d482756cf05a
Allison Wagner <allisonjw01@gmail.com>
2020-06-02 15:14:01 -0600

    Moves api fetch from App to Container

--- src/App/App.js
+++ src/App/App.js
@@ -1,70 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch  } from 'react-redux';
+import React from 'react';
 import './App.scss';
-import { getProducts } from '../Util/apiCall';
-import { addFoundations, addEyeShadows, addBlushes, addBronzers, addLipsticks, addMascaras, isLoading } from '../actions';
 import Nav from '../Nav/Nav';
 import Categories from '../Categories/Categories';
 import Container from '../Container/Container';
-// import { Product } from '../Product/Product';
 import { Route, Switch } from 'react-router-dom';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const [foundation, setAllFoundations] = useState([]);
-  const [eyeShadow, setAllEyeShadows] = useState([]);
-  const [blush, setAllBlushes] = useState([]);
-  const [bronzer, setAllBronzers] = useState([]);
-  const [lipstick, setAllLipsticks] = useState([]);
-  const [mascara, setAllMascaras] = useState([]);
-  const [loading, isLoading] = useState(true);
-
-  const fetchProducts = async () => {
-      try {
-          isLoading(true);
-          const foundations = await getProducts('foundation');
-          setAllFoundations(foundations);
-          dispatch(addFoundations(foundations));
-
-          const eyeShadows = await getProducts('eyeshadow');
-          setAllEyeShadows(eyeShadows);
-          dispatch(addEyeShadows(eyeShadows));
-
-          const blushes = await getProducts('blush');
-          setAllBlushes(blushes);
-          dispatch(addBlushes(blushes));
-
-          const bronzers = await getProducts('bronzer');
-          setAllBronzers(bronzers);
-          dispatch(addBronzers(bronzers));
-
-          const lipsticks = await getProducts('lipstick');
-          setAllLipsticks(lipsticks);
-          dispatch(addLipsticks(lipsticks));
-
-          const mascaras = await getProducts('mascara');
-          setAllMascaras(mascaras);
-          dispatch(addMascaras(mascaras));
-
-        } catch({ message }) {
-            isLoading(false)
-            console.log(message);
-        }
-  };
-
-    useEffect(() => {
-      fetchProducts();
-    }, []);
-
-    const props = (foundation, mascara, eyeShadow, bronzer, blush, lipstick)
     return (
       <>
+      <Container />
       <Switch >
-        <Route exact path='/' render={() => <> <Nav /> <Categories /> </>} />
-        {/* <Route path='/products' component={Product} /> */}
-        <Route exact path='/products/:type' render={({match}) => {
-            let productType = Object.keys(props).find(type => type === match.params.type)
-          return <> <Nav /><Container productType={props[productType]} toggleCollection={this.toggleCollection} /> </>}}/>
+        <Route exact path='/' render={() => <> <Nav /> </>} />
+        <Route path='./container' render={() => <>  <Nav /> <Container /> </>} />
+        <Route path='./categories' render={() => <>  <Nav /> <Categories /> </>} />
      </Switch >
      </>
     );
--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,43 +1,71 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Product from '../Product/Product';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getProducts } from '../Util/apiCall';
+import { addFoundations, addEyeShadows, addBlushes, addBronzers, addLipsticks, addMascaras, isLoading } from '../actions';
 import './Container.scss';
+import { Route } from 'react-router-dom';
+import Categories from '../Categories/Categories';
+import Nav from '../Nav/Nav';
 
-const Container = ({productType, type, collection }) => {
-    const allFoundations = useSelector(state => state.foundations)
-    const allEyeShadows = useSelector(state => state.eyeShadows)
-    const allBlushes = useSelector(state => state.blushes)
-    const allBronzers = useSelector(state => state.bronzers)
-    const allLipsticks = useSelector(state => state.lipsticks)
-    const allMascaras = useSelector(state => state.mascaras)
+export const Container = () => {
+    const dispatch = useDispatch();
+    const [foundation, setAllFoundations] = useState([]);
+    const [eyeShadow, setAllEyeShadows] = useState([]);
+    const [blush, setAllBlushes] = useState([]);
+    const [bronzer, setAllBronzers] = useState([]);
+    const [lipstick, setAllLipsticks] = useState([]);
+    const [mascara, setAllMascaras] = useState([]);
+    const [loading, isLoading] = useState(true);
+  
+    const fetchProducts = async () => {
+        try {
+            isLoading(loading);
+            const foundations = await getProducts('foundation');
+            setAllFoundations(foundations);
+            dispatch(addFoundations(foundations));
+  
+            const eyeShadows = await getProducts('eyeshadow');
+            setAllEyeShadows(eyeShadows);
+            dispatch(addEyeShadows(eyeShadows));
+  
+            const blushes = await getProducts('blush');
+            setAllBlushes(blushes);
+            dispatch(addBlushes(blushes));
+  
+            const bronzers = await getProducts('bronzer');
+            setAllBronzers(bronzers);
+            dispatch(addBronzers(bronzers));
+  
+            const lipsticks = await getProducts('lipstick');
+            setAllLipsticks(lipsticks);
+            dispatch(addLipsticks(lipsticks));
+  
+            const mascaras = await getProducts('mascara');
+            setAllMascaras(mascaras);
+            dispatch(addMascaras(mascaras));
+  
+          } catch({ message }) {
+              isLoading(false)
+              console.log(message);
+          }
+    };
+  
+      useEffect(() => {
+        fetchProducts();
+      }, []);
+  
+    let allStates = {foundation, eyeShadow, blush, bronzer, lipstick, mascara};
 
-    let allCategories = [...allFoundations, ...allEyeShadows, ...allBlushes, ...allBronzers, ...allLipsticks, ...allMascaras]
-
-  if (type === 'collection') {
-    let collectionList = collection.map(product => <Product key={product.id} product={product} /> );
+    let allCategoriesArray = [...foundation, ...eyeShadow, ...blush, ...bronzer, ...lipstick, ...mascara];
 
     return (
-      <main className='Container'>
-        {collectionList}
-      </main>
-    )
-  } else if (type === 'shopall') {
-    let allCategoriesList = allCategories.map(product => <Product key={product.id} product={product} />);
-
-    return (
-      <main className='Container'>
-        {allCategoriesList}
-      </main>
-    )
-  } else {
-    const productList = productType.map(product => <Product key={product.id}product={product} />);
-
-    return (
-      <main className = 'Container'>
-        {productList}
-      </main>
+      <>
+        <Route exact path='/products/:type' render={({match}) => {
+            let productType = Object.keys(allStates).find(type => type === match.params.type)
+        return <> <Nav /> <Categories productType={allStates[productType]} /> </>}}/>  
+        <Route exact path='/shopall' render={() => <><Nav /><Categories type='shopall' allCategoriesArray={allCategoriesArray} /> </>} />    
+      </>
     )
   }
-}
 
 export default Container;
